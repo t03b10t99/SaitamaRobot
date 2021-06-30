8e772fe88f86d9feb4b2cef9e05173d87e6152ac
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-06-30 20:54:09 +0700

    Create musicplayer.py

---
+++ SaitamaRobot/modules/musicplayer.py
@@ -0,0 +1,39 @@
+__mod_name__ = "Music Player"
+
+__help__ = """
+🛠️ Settings
+
+1) Make Bot As Admin
+2) Start Voice Chat / VCG
+3) Send Command /userbotjoin
+• If Assistant Bot Joins Enjoy Music,
+• If Assistant Bot Not Joined Please Add @AssistantMusicPlayer To Your Group And Try Again
+
+📍 Command 📍
+
+=>> Playing Song  🎧
+
+• /play (song name) - To Play the song you requested via youtube
+• /dplay (song name) - To Play the song you requested via deezer
+• /splay (song name) - To Play the song you requested via jio saavn
+• /ytplay (song name) - To Play the song you requested via youtube in more detail
+
+==> Song Downloads 🎶
+
+• /song (nama lagu) : Untuk Mendownload lagu di YouTube 
+• /vsong (nama lagu) : Untuk Mendownload Video di YouTube dengan detail
+• /deezer (nama lagu) : Untuk Mendownload lagu dari deezer 
+• /saavn (nama lagu) : Untuk Mendownload lagu dari website saavn
+
+=>> Playback ⏯
+
+• /player: Open the Player settings menu
+• /skip: Skips the current track
+• /pause: Pause track
+• /resume: Resume a paused track
+• /end: ​​Stops media playback
+• /current: Displays the currently playing track
+• /playlist: Displays a playlist
+
+All Commands Can Be Used Except Command /player /skip /pause /resume /end Only For Group Admins
+"""
