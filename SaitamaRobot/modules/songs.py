3912c02c7f4c35c182a9d7c03ec8d4862f81fb4b
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-07-01 18:42:57 +0700

    Create songs.py

---
+++ SaitamaRobot/modules/songs.py
@@ -0,0 +1,8 @@
+__mod_name__ = "Music"
+
+__help__ = """
+• /song (song name) : To Download songs on YouTube
+• /vsong (song name) : To Download Videos on YouTube with details
+• /deezer (song name) : To Download songs from deezer
+• /saavn (song name) : To Download songs from saavn . website
+"""
