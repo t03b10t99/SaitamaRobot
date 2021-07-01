1bb75232fc24b414457631709fc0ddb9e4df69f7
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-07-01 19:44:38 +0700

    Delete songs.py

--- SaitamaRobot/modules/songs.py
+++
@@ -1,8 +0,0 @@
-__mod_name__ = "Music"
-
-__help__ = """
-• /song (song name) : To Download songs on YouTube
-• /vsong (song name) : To Download Videos on YouTube with details
-• /deezer (song name) : To Download songs from deezer
-• /saavn (song name) : To Download songs from saavn . website
-"""
